e8cf773e4d3fd8b06d56437a36f99e4a5e5bf87b
yixiaohai <86066331@qq.com>
2025-02-03 06:22:25 +0800

    再重新传一下

--- addon/game/scripts/vscripts/addon_game_mode.lua
+++
@@ -1,6 +0,0 @@
-local ____lualib = require("lualib_bundle")
-local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 2,["5"] = 3,["6"] = 2});
-function test(self)
-    print("test123")
-end
--- src/vscripts/addon_game_mode.ts
+++
@@ -1,4 +0,0 @@
-// 连接GameMode.Activate和GameMode.Precache到dota引擎
-function test() {
-    print('test123');
-}
